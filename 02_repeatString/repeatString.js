a0c9fa5ba18e131e52919576b5184ef4973e52e5
Exequiel Yuri <leiuqxre@gmail.com>
2022-03-21 18:48:17 +0800

    Finish 02-repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+/*
+PSEUDO
+- input: string & number
+- make a new string
+    - string * num
+*/
 
+const repeatString = function(string, num) {
+    if (num<0) return 'ERROR'; // can't repeat negative times
+    
+    let repeatedString = '';
+
+    for (let i=0; i<num; i++) { // append self `num` times
+        repeatedString += string; 
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
