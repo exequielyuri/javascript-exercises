1b4a38b7377c1cb925d8c93ff9d78d4f524b533a
Exequiel Yuri <leiuqxre@gmail.com>
2022-03-21 18:56:27 +0800

    Finish 03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,23 @@
-const reverseString = function() {
+/*
+PSEUDO
+- get input string
+- put each letter in an array
+    - using split('')
+- create new string variable
+- append each array element in descending
+    - use array.length-1 to get scope
+    - use for loop
+*/
 
+const reverseString = function(str) {
+    const letters = str.split('');
+    const lastIndex = letters.length-1;
+    let reversedString = '';
+
+    for (let i=lastIndex; i>=0; i--) { //starting from last
+        reversedString += letters[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
