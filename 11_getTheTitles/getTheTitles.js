7cfed9c40beb363aa855427ef1def4e2590a04bc
Exequiel Yuri <leiuqxre@gmail.com>
2022-03-22 13:56:40 +0800

    Finish 11-getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,19 @@
-const getTheTitles = function() {
+/*
+PSEUDO
+get input: array of objects
+extract all titles
+    - foreach object
+        - access object[title]
+put those titles in a new array
+    - push()
+*/
 
+const getTheTitles = function(books) {
+    const titles = [];
+    for (const book of books) {
+        titles.push(book['title']);
+    }
+    return titles;
 };
 
 // Do not edit below this line
