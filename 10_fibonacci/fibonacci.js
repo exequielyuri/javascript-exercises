00ec69b0afb88e22824a005878a99a2e8cfcaed2
Exequiel Yuri <leiuqxre@gmail.com>
2022-03-22 13:50:39 +0800

    Finish 10-fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+/*
+PSEUDO
+get input: position in fibonacci
+if 1st position, return 1
+if 2nd position, return 1
+F(3), return fibonacci(1) + fibonacci(2)
+*/
 
+const fibonacci = function(num) {
+    if (num<0) { // error for negative input
+        return 'OOPS';
+    }
+
+    if (num < 3) { // base case for 1st & 2nd position
+        return 1;
+    }
+
+    return fibonacci(num-2) + fibonacci(num-1);
 };
 
 // Do not edit below this line
