969bf3786a89e9a9eb6b3f8f3439cc5728550d89
Exequiel Yuri <leiuqxre@gmail.com>
2022-03-22 09:40:20 +0800

    Finish 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let result = 0;
+  for (let num of numbers) {
+    result += num;
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let result = 1;
+  for(let num of numbers) {
+    result *= num;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 2) return 1; // special case for 0 & 1
+
+  let result = 1;
+  while (num > 1) {
+    result *= num;
+    num--;
+  }
+  return result;
 };
 
 // Do not edit below this line
