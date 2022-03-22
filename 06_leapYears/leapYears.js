337213f531ceabbab95693f8910dcda8f9ee1d4c
Exequiel Yuri <leiuqxre@gmail.com>
2022-03-22 08:56:36 +0800

    Finish 06-leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
+/*
+PSEUDO
+- get input: year
+- if divisible by 400
+    - true
+- or if divisible 4 and NOT divisible by 100
+    - true
+*/
 
+const leapYears = function(year) {
+    let isDivBy4 = year%4 === 0;
+    let isDivBy100 = year%100 === 0;
+    let isDivBy400 = year%400 === 0;
+
+    if (isDivBy4 && !isDivBy100 || isDivBy400) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
