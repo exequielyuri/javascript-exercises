55d1aeb3359f9b378126f7bf147918d41ac159b4
Exequiel Garcia <leiuqxre@gmail.com>
2022-04-17 18:03:57 +0800

    Finish 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+/*
+    get input of person object
+    get their age
+    sort
+    then return highest
+*/
+function computeAge(person) {
+    if (!person.yearOfDeath) { // if alive
+        person.yearOfDeath = (new Date()).getFullYear();
+    }
+    return person.age = person.yearOfDeath - person.yearOfBirth;
+}
 
+function sortByAge(a, b) {
+    return (a.age > b.age) ? -1 : 1; // descending
+}
+
+const findTheOldest = function(persons) {
+    persons.forEach(computeAge);
+    persons.sort(sortByAge);
+    return persons[0];
 };
 
 // Do not edit below this line
